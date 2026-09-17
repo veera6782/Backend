9a5e3b54cb5646a994feb2c6d29d7a072926578f
sathyat20 <sathya.thenappan.23@dartmouth.edu>
2026-09-17 19:06:45 +0800

    mongodb changes

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.get('/api/health', (req, res) => {
   res.json({ status: 'ok' })
 })
 app.use('/api/users', userRoutes)
+app.use('/api/profile', userRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
